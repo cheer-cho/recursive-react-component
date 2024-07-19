98b68f32a7eee54e6cb6a386a1f1b3ec955529ec
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2024-07-19 17:21:02 -0500

    [8] - add and config vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    css: true,
+    globals: true,
+  },
+});
